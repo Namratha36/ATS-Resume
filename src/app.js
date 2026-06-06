fb256df8912cbd7dd5e71f235c4fc6867a34ecaa
Namratha <namrathajyoshika369@gmail.com>
2026-06-06 22:03:59 +0530

    Save latest workspace changes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
-const expess = require('express');
+const express = require('express');
 
 
 const app = express();
 
 app.use(express.json());
+
+
+// require all the routes here
+const authRouter = require('./routes/auth.routes');
+
+
+// using all the routes here
+app.use('/api/auth', authRouter);
+
+
+/**
+ * @route POST /api/auth/register
+ * @desc Register a new user
+ * @access Public
+ */
+
+authRouter
 
 module.exports = app;
